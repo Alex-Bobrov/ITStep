8abc500cd64697a3e39e89646c5fd7da13c72065
Alex Bobrov <b-aleks05076969@mail.ru>
2022-11-01 21:22:34 +0300

    29

---
+++ Tasks/Task 29. Array/app2.js
@@ -0,0 +1,31 @@
+// #1
+
+let a = [0, 1, 2, 3, 4, 5, 8];
+
+console.log("Task 1", a);
+
+// #4
+
+let b = [0, 1, 2, 3, 4, 5, 8];
+
+let sum = 0;
+
+for (let i = 0; i < b.length; i++) {
+    sum += b[i]
+}
+console.log("Task 4", sum);
+
+// #5
+
+let c =['a', 'b', 'c', 'd'];
+
+let str = "";
+
+for (let i = 0; i < c.length - 1; i++) {
+    str = str + c[i] + "+";
+}
+str = str + c[c.length - 1];
+console.log("Task 5", str);
+
+// #6
+
